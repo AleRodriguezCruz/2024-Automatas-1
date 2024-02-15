3cfaff339b0e20c9850f42a4525afa7b4e6a9213
Ale Ro <alejandrarodriguez@corp.visionairelighting.com>
2024-02-14 18:18:43 -0800

    Se agrega el Proyecto Practica 1

---
+++ dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id_matricula] INT NOT NULL PRIMARY KEY, 
+    [Nombre] NCHAR(50) NOT NULL, 
+    [Apellido_P] NCHAR(50) NOT NULL, 
+    [Apellido_M] NCHAR(50) NOT NULL, 
+    [Carrera] NCHAR(50) NOT NULL
+)
