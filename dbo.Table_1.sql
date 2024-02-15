e940d35595f6928aa1d057a6e707aae2a3723829
Ale <156391201+AleRodriguezCruz@users.noreply.github.com>
2024-02-14 18:45:23 -0800

    Delete dbo.Table_1.sql

--- dbo.Table_1.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Alumno]
-(
-	[Id_Matricula] INT NOT NULL PRIMARY KEY, 
-    [Nombre] NCHAR(50) NOT NULL, 
-    [Apellido_P] NCHAR(50) NOT NULL, 
-    [Apellido_M] NCHAR(50) NOT NULL, 
-    [Carrera] NCHAR(50) NOT NULL
-)
